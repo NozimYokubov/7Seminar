19bc0fa1bc08fe07150b873fc5e99e4e1143a82e
Nozim Yokubov <anorcomander@gmail.com>
2024-04-14 19:07:02 +0500

    2я задача в классе 7го семинара по рекурсии

---
+++ Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет 
+// принимать на вход число и возвращать 
+// сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+
+// Console.WriteLine("Введите натуральное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int number) // 123 12 1 0
+{
+	if(number == 0) return 0; // пропускается 
+	return number % 10 + SumDigits(number / 10); // 123 12 1 0
+    // 1%10 + 12%10 + 123%10 + 0 = 6
+}
+
+Console.WriteLine(SumDigits(123)); // 6
+
+
+
+// void PrintNaturalNumbers(int num)
+// {
+//     if ( num == 0) return;
+//     // Console.Write($"{num} ");  //Выводит 5 4 3 2 1
+//     PrintNaturalNumbers(num - 1);
+//     Console.Write($"{num} ");     //Выводит 1 2 3 4 5
+// }
+
+// PrintNaturalNumbers(number);
